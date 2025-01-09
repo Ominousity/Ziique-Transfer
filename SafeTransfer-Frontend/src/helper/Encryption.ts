8a00b45dfed5adf7b010821d1525aef5258ea742
Anders Senger <91528409+Ominousity@users.noreply.github.com>
2025-01-09 17:58:56 +0100

    Enhance decryption method by adding IV specification for improved security; adjust key handling

--- SafeTransfer-Frontend/src/helper/Encryption.ts
+++ SafeTransfer-Frontend/src/helper/Encryption.ts
@@ -13,7 +13,14 @@ export function encrypt(data: string, key: string): string {
 }
 
 export function decrypt(data: string, key: string): string {
-    const bytes = CryptoJS.AES.decrypt(data, key);
+    let secSpec = CryptoJS.enc.Utf8.parse(key);
+    let ivSpec = CryptoJS.enc.Utf8.parse(key);
+
+    secSpec = CryptoJS.lib.WordArray.create(secSpec.words.slice(0, 16/4));
+    ivSpec = CryptoJS.lib.WordArray.create(secSpec.words.slice(0, 16/4));
+
+    const bytes = CryptoJS.AES.decrypt(data, key,  {iv: ivSpec});
+    
     const originalText = bytes.toString(CryptoJS.enc.Utf8);
     return originalText;
 }
